ca7b0f92c568cdb16ea40874cb507077c652c857
Jim Graszer <jim@lemmalegal.com>
2024-10-25 14:30:57 -0500

    Fixes to the display for preparer address depending on selection, automated tests.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.USCISApplications',
-      version='10.2.6',
+      version='10.2.5',
       description=('A docassemble extension.'),
       long_description='# docassemble.USCISApplications\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nAnthony Danielson, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
